2e38c57945721db4ba516283ba2f1de01f993f2f
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-23 08:23:38 +0545

    intregrate docker secrets

--- index.ts
+++ index.ts
@@ -1,46 +1,56 @@
-import express, { Request, Response } from 'express';
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 import { getHostName, getVersion } from "./utils/index.js";
-
+import fs from "node:fs";
 dotenv.config();
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+let dbUser = process.env.DB_USER;
+let dbPASSWORD = process.env.DB_PASSWORD;
+if (!dbUser && process.env.DB_USER_USER_FILE) {
+  dbUser = fs.readFileSync(process.env.DB_USER_USER_FILE, "utf8").trim();
+}
+
+if (!dbPASSWORD && process.env.DB_USER_FILE) {
+  dbPASSWORD = fs.readFileSync(process.env.DB_USER_FILE, "utf8").trim();
+}
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: dbUser,
+  password: dbPASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Root path: simple welcome message
-app.get('/', (req, res) => {
-    res.send('Welcome to Blog');
+app.get("/", (req, res) => {
+  res.send("Welcome to Blog");
 });
 
 // /blogs path: fetch and render blog data
-app.get('/blogs', async (req, res) => {
-    try {
-        const [rows] = await pool.query('SELECT title, description FROM blogs');
-        res.render('index', {
-            blogs: rows,
-            version: getVersion(),
-            host: process.env.NODE_HOST || getHostName(),
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error retrieving blog posts');
-    }
+app.get("/blogs", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT title, description FROM blogs");
+    res.render("index", {
+      blogs: rows,
+      version: getVersion(),
+      host: process.env.NODE_HOST || getHostName(),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving blog posts");
+  }
 });
 
-app.get('/health', (req: Request, res: Response) => {
-    console.log('Checking health through endpoint...');
-    res.json({ alive: true });
+app.get("/health", (req: Request, res: Response) => {
+  console.log("Checking health through endpoint...");
+  res.json({ alive: true });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
